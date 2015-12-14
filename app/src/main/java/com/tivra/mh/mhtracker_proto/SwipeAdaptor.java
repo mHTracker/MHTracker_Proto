7fd1f329b821343d758ec1cfa298212ceb7d7cb2
Ravi <mh.inovation@gmail.com>
2015-12-14 14:51:06 +0530

    testing

--- app/src/main/java/com/tivra/mh/mhtracker_proto/SwipeAdaptor.java
+++ app/src/main/java/com/tivra/mh/mhtracker_proto/SwipeAdaptor.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 import view.FirstFragment;
+import view.SecondFragment;
 
 /**
  * Created by Ravinder.Singroha on 12/11/2015.
@@ -18,13 +19,20 @@ public class SwipeAdaptor extends FragmentStatePagerAdapter {
     public Fragment getItem(int position) {
         Fragment fragment =new FirstFragment();
         Bundle bundle =new Bundle();
-        bundle.putInt("count",position+0);
+        bundle.putInt("count", position + 0);
         fragment.setArguments(bundle);
+        if (position==3) {
+            Fragment fragment1 =new SecondFragment();
+           // Bundle bundle1 =new Bundle();
+          //  bundle1.putInt("count", position + 0);
+            fragment.setArguments(bundle);
+            return fragment1;
+        }
         return fragment;
     }
 
     @Override
     public int getCount() {
-        return 5;
+        return 4;
     }
 }
