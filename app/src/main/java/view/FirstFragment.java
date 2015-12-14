059af3561adc81eadccf19bdcc96cd8b32c3dbb8
Ravi <mh.inovation@gmail.com>
2015-12-14 14:51:21 +0530

    testing

--- app/src/main/java/view/FirstFragment.java
+++ app/src/main/java/view/FirstFragment.java
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tivra.mh.mhtracker_proto.R;
 
 public class FirstFragment extends Fragment {
 TextView   textView;
+    ImageView image_view1;
     public FirstFragment() {
         // Required empty public constructor
     }
@@ -25,10 +27,21 @@ TextView   textView;
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_first, container, false);
-        textView= (TextView)view.findViewById(R.id.text_view);
+        image_view1 =(ImageView)view.findViewById(R.id.image_view);
+        //textView= (TextView)view.findViewById(R.id.text_view);
         Bundle bundle = getArguments();
         String msg= Integer.toString(bundle.getInt("count"));
-        textView.setText("This is the "+ msg + "Swipe Page view...");
+        if (bundle.getInt("count")==0)
+        {
+        image_view1.setImageResource(R.drawable.first);
+        }
+        else if (bundle.getInt("count")==1) {
+            image_view1.setImageResource(R.drawable.second);
+        }
+        else if (bundle.getInt("count")==2) {
+            image_view1.setImageResource(R.drawable.third);
+        }
+       // textView.setText("This is the "+ msg + "Swipe Page view...");
         return view;
 //        return inflater.inflate(R.layout.fragment_first, container, false);
     }
